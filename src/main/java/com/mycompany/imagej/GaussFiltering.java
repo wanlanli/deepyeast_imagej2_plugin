d8cec95fd05b11307de5bdabaac937190d6e6231
wanlanli <wanlanli.ky@gmail.com>
2023-02-03 18:07:12 +0100

    image to tensor

--- src/main/java/com/mycompany/imagej/GaussFiltering.java
+++ src/main/java/com/mycompany/imagej/GaussFiltering.java
@@ -11,20 +11,37 @@ package com.mycompany.imagej;
 import net.imagej.Dataset;
 import net.imagej.ImageJ;
 import net.imagej.ops.OpService;
+import net.imagej.tensorflow.Tensors;
+
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
 import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.FloatType;
+
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.ui.UIService;
 
 import org.tensorflow.Tensor;
+//import org.tensorflow.ndarray.Shape;
+//import org.tensorflow.ndarray.buffer.ByteDataBuffer;
+//import org.tensorflow.op.Ops;
+//import org.tensorflow.types.TFloat16;
+//import org.tensorflow.types.TFloat32;
+//import org.tensorflow.types.TString;
+//import org.tensorflow.ndarray.ByteNdArray;
+//import org.tensorflow.ndarray.NdArrays;
+//import org.tensorflow.ndarray.buffer.DataBuffers;
+//import org.tensorflow.op.image.DecodeImage;
+//import org.tensorflow.op.Scope;
+//import org.tensorflow.Graph;
 
+//import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
+import net.imglib2.type.numeric.real.FloatType;
 /**
  * This example illustrates how to create an ImageJ {@link Command} plugin.
  * <p>
@@ -49,18 +66,41 @@ public class GaussFiltering<T extends RealType<T>> implements Command {
 
     @Parameter
     private OpService opService;
- 
+    
     @Override
     public void run() {
         final Img<T> image = (Img<T>)currentData.getImgPlus();
-
-        //
+        System.out.print(image.dimension(0));
+        System.out.print(image.dimension(1));
+ 
+        System.out.print("aaaa");
+        // Tensor input_tensor = TensorManagement.imPlus2tensor(image, "HW");
+        
+		int[] dimOrder = new int[2];
+		for (int i = 0; i < 2; i++) {
+			dimOrder[i] = 512;
+		}
+		System.out.print("\nbbbbbb");
+		Tensor input_tensor = Tensors.tensor(image, dimOrder);
+		System.out.print("\nccccc");
+		System.out.print(input_tensor);
+//        Graph graph = new Graph();
+//        Scope scope = new Scope(graph);
+//        DecodeImage.create(scope, image.toString(), TFloat16.class);
+//        Tensor a =tf.constant(image.toString()).asTensor();
+//        Tensor a = tf.reshape(tf.constant(image.toString()), tf.array(image.size())).asTensor();
+//        		tf.reshape(
+//        		tf.dtypes.cast(tf.constant(image.toString()), TString.class),
+//        		tf.array(-1L, image.size())
+//        		).asTensor();
+ //       System.out.print(a);
+        
         // Enter image processing code here ...
         // The following is just a Gauss filtering example
         //
-        HelloTensorFlow model = new HelloTensorFlow();
-        
-        // Tensor input_tensor = Tensor.create(image);
+        // HelloTensorFlow model = new HelloTensorFlow();
+        // TFloat16 test = TFloat16.tensorOf(image.);
+        // Tensor input_tensor = Tensor.of(image.toString());
         // Tensor out = model.predict(input_tensor);
         
         final double[] sigmas = {1.0, 3.0, 5.0};
